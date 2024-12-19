15cfde6878437709d9f7a3662c03cb2bc89491b8
AntoninoAdornetto <armaninollc@protonmail.com>
2024-12-18 23:20:23 -0700

    chore(header): cfg prototypes & type defs

--- include/util.h
+++ include/util.h
@@ -1,6 +1,22 @@
 #ifndef UTIL_H
 #define UTIL_H
+#include <stdint.h>
 
-void usage();
+typedef uint32_t u32;
+typedef uint16_t u16;
+typedef uint8_t u8;
+
+/*
+ * [paths] houses the file paths and dir paths that the daemon will monitor.
+ * [flags] bit flags that indicate what events the daemon will trigger
+ * notifications.
+ */
+typedef struct {
+  char **paths;
+  u16 flags;
+} WardenConfig;
+
+WardenConfig *process_config(void);
+void free_config(WardenConfig *cfg);
 
 #endif
