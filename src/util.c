c2a0b4929bc8682c456ab6ffa52d2825d579a4af
AntoninoAdornetto <armaninollc@protonmail.com>
2024-12-18 23:19:51 -0700

    docs(todo): reported todo to ghub via issue summoner

--- src/util.c
+++ src/util.c
@@ -1,6 +1,13 @@
 #include "util.h"
 #include <stdlib.h>
 
+/*
+ * @TODO:(#1) Handle Config paths array memory allocation/deallocation
+ * the paths array is not utilized at the moment. I am determining how I want
+ * to design the system and will circle back to it once I am further along in
+ * development
+ */
+
 WardenConfig *process_config(void) {
   WardenConfig *cfg = (WardenConfig *)malloc(sizeof(WardenConfig));
   return cfg;
