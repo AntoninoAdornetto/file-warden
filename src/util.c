31c79fe1d42db484ad24ae90038bd17dcb587de1
AntoninoAdornetto <armaninollc@protonmail.com>
2024-12-20 02:17:21 -0700

    fix: issue #1 (paths allocation/clean up)

--- src/util.c
+++ src/util.c
@@ -1,13 +1,6 @@
 #include "util.h"
 #include <sys/stat.h>
 
-/*
- * @TODO:(#1) Handle Config paths array memory allocation/deallocation
- * the paths array is not utilized at the moment. I am determining how I want
- * to design the system and will circle back to it once I am further along in
- * development
- */
-
 int file_exists(const char *filename) {
   struct stat buf;
   return (stat(filename, &buf) == 0);
