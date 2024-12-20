ffb11f4cf5d04b65d333b654fee4f3fa58e98258
AntoninoAdornetto <armaninollc@protonmail.com>
2024-12-20 02:15:39 -0700

    refactor: mv some prototypes to new config header file

--- include/util.h
+++ include/util.h
@@ -2,21 +2,11 @@
 #define UTIL_H
 #include <stdint.h>
 
-typedef uint32_t u32;
-typedef uint16_t u16;
 typedef uint8_t u8;
 
 /*
- * [paths] houses the file paths and dir paths that the daemon will monitor.
- * [flags] bit flags that indicate what events the daemon will trigger
- * notifications.
+ * Convenience function
  */
-typedef struct {
-  char **paths;
-  u16 flags;
-} WardenConfig;
-
-WardenConfig *process_config(void);
-void free_config(WardenConfig *cfg);
+int file_exists(const char *filename);
 
 #endif
