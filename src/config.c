54cd4e81828fb98faae9ca17d59e644dce0f225a
AntoninoAdornetto <armaninollc@protonmail.com>
2024-12-25 01:39:51 -0700

    fix: re-direct stderr,stdout to journalctl

--- src/config.c
+++ src/config.c
@@ -4,10 +4,13 @@
 #include <stdlib.h>
 #include <string.h>
 #include <sys/stat.h>
+#include <sys/syslog.h>
 
 /* Configuration options (tbd if more will be added) */
 static const char *OPT_PATHS = "paths";
 static const char *OPT_EVENTS = "events";
+
+/* Permitted file events for [OPT_EVENTS] option setting */
 static const char *EVENT_ACCESS = "accessed";
 static const char *EVENT_MODIFY = "modified";
 static const char *EVENT_MOVE = "moved";
@@ -24,33 +27,33 @@ static const char *CFG_HOME_PATH = "~/.file-warden.conf";
 Config *init_config(void) {
   Config *cfg = (Config *)malloc(sizeof(Config));
   if (cfg == NULL) {
-    fprintf(stderr, "[ERROR]: Failed to allocate memory for config struct\n");
+    syslog(LOG_ERR, "Failed to allocate memory for config struct\n");
     exit(EXT_CFG_ALLOC);
   }
 
   cfg->paths_size = 0;
   cfg->paths = (char **)malloc(sizeof(char *));
   if (cfg->paths == NULL) {
-    fprintf(stderr, "[ERROR]: Failed to allocate memory for paths property\n");
+    syslog(LOG_ERR, "Failed to allocate memory for watch paths\n");
     free_config(cfg);
     exit(EXT_CFG_ALLOC);
   }
 
   char *program_settings = get_config_settings(cfg);
   if (program_settings == NULL) {
-    fprintf(stderr, "[ERROR]: Failed to get option settings\n");
+    syslog(LOG_ERR, "Failed to fetch program option settings\n");
     free_config(cfg);
     exit(EXT_NULL_CFG);
   }
 
   int result = process_settings(cfg, program_settings);
   if (result != 0) {
-    fprintf(stderr, "[ERROR]: Failed to process program settings\n");
+    syslog(LOG_ERR, "Failed to process program option settings\n");
     free_config(cfg);
     exit(result);
   }
 
-  if (cfg->config_loc & (CFG_LOC_HOME | CFG_LOC_ETC)) {
+  if (cfg->config_location & (CFG_LOC_HOME | CFG_LOC_ETC)) {
     free(program_settings);
   }
 
@@ -76,40 +79,37 @@ char *get_config_settings(Config *cfg) {
   // we should use the home configuration first since it's user specific
   // and defer to system configuration if home file is not present.
   if (in_home || in_etc) {
-    cfg->config_loc |= in_home ? CFG_LOC_HOME : CFG_LOC_ETC;
+    cfg->config_location |= in_home ? CFG_LOC_HOME : CFG_LOC_ETC;
 
     char *settings = read_file(in_home ? CFG_HOME_PATH : CFG_ETC_PATH);
     if (settings != NULL) {
-      fprintf(stdout, "[INFO]: Using config at [%s]\n",
-              in_home ? CFG_HOME_PATH : CFG_ETC_PATH);
+      syslog(LOG_INFO, "Using config file at [%s]\n",
+             in_home ? CFG_HOME_PATH : CFG_ETC_PATH);
+
       return settings;
     }
 
-    fprintf(stdout,
-            "[WARNING]: Failed to read settings from config file at [%s]. "
-            "Using default settings as fallback\n",
-            in_home ? CFG_HOME_PATH : CFG_ETC_PATH);
+    syslog(LOG_WARNING, "Failed to read setting from config file. Deferring to "
+                        "default settings\n");
   }
 
-  fprintf(
-      stdout,
-      "[WARNING]: Configuration files do not exist. \n[INFO]: Please create "
-      "or copy the example config to [/etc/file-warden.conf] or "
-      "[~/.file-warden.conf].\n[INFO]: Deferring to default settings\n\n");
+  syslog(LOG_WARNING,
+         "Config files may not exist.\nPlease create or copy the example "
+         "config.\nDeferring to sensible default settings");
 
-  // Sensible defaults we will set if the config files are not present.
-  cfg->config_loc |= CFG_LOC_DEFAULT;
+  // Sensible default if the config files are not present.
+  cfg->config_location |= CFG_LOC_DEFAULT;
   return "paths=~/.ssh\nevents=accessed,modified,moved,closed\n";
 }
 
 int process_settings(Config *cfg, const char *settings) {
   if (cfg == NULL) {
-    fprintf(stderr, "[ERROR]: Expected config struct not to be null\n");
+    syslog(LOG_ERR, "Expected config struct not to be null\n");
     return EXT_NULL_CFG;
   }
 
   if (settings == NULL) {
-    fprintf(stderr, "[ERROR]: Expected program settings not to be empty\n");
+    syslog(LOG_ERR, "Expected program settings not to be empty\n");
     return EXT_NULL_CFG;
   }
 
@@ -133,8 +133,7 @@ int process_line_option(Config *cfg, char *line) {
 
   char *equal_sign = strchr(line, '=');
   if (equal_sign == NULL) {
-    fprintf(stderr,
-            "[ERROR]: Invalid option format. Expected \"option=val1,val2\"\n");
+    syslog(LOG_ERR, "Invalid option format. Expected \"option=val1,val2\"\n");
     return EXT_OPT_FORMAT;
   }
 
@@ -142,7 +141,7 @@ int process_line_option(Config *cfg, char *line) {
   key[equal_sign - line] = '\0';
   u8 opt_flag = validate_option(key);
   if (opt_flag == FLAG_INVAL_OPT) {
-    fprintf(stdout, "[WARNING]: Invalid option setting [%s]\n", key);
+    syslog(LOG_WARNING, "Invalid option setting [%s]\n", key);
   }
 
   strcpy(values, equal_sign + 1);
@@ -164,43 +163,40 @@ u8 validate_option(char *setting_key) {
     return FLAG_EVENTS_OPT;
   }
 
+  syslog(LOG_WARNING, "Invalid option setting with value [%s]\n", setting_key);
   return 0;
 }
 
-void set_option(Config *cfg, u8 option_flag, char *value) {
-  if (option_flag & FLAG_INVAL_OPT) {
-    return;
-  }
-
+int set_option(Config *cfg, u8 option_flag, char *value) {
   if (option_flag & FLAG_PATHS_OPT) {
-    set_paths_option(cfg, value);
-    return;
+    return set_paths_option(cfg, value);
   }
 
   if (option_flag & FLAG_EVENTS_OPT) {
-    set_events_option(cfg, value);
-    return;
+    return set_events_option(cfg, value);
   }
+
+  syslog(LOG_WARNING, "skipping value [%s]. Unknown option setting\n", value);
+  return 0;
 }
 
-void set_paths_option(Config *cfg, char *value) {
+int set_paths_option(Config *cfg, char *value) {
   char *cleaned_path = expand_path(value);
+
   cfg->paths[cfg->paths_size] = malloc(sizeof(char) * strlen(cleaned_path) + 1);
   if (cfg->paths[cfg->paths_size] == NULL) {
-    fprintf(
-        stderr,
-        "[ERROR]: Failed to allocate memory for paths value at index [%d]\n",
-        cfg->paths_size);
-    free_config(cfg);
-    return;
+    syslog(LOG_ERR, "Failed to allocate memory for path element at index [%d]",
+           cfg->paths_size);
+    return EXT_CFG_ALLOC;
   }
 
   strcpy(cfg->paths[cfg->paths_size], cleaned_path);
   free(cleaned_path);
   cfg->paths_size++;
+  return 0;
 }
 
-void set_events_option(Config *cfg, char *value) {
+int set_events_option(Config *cfg, char *value) {
   const FileEventMapping events[] = {{EVENT_ACCESS, FLAG_ACCESS},
                                      {EVENT_MODIFY, FLAG_MODIFY},
                                      {EVENT_MOVE, FLAG_MOVE},
@@ -213,45 +209,47 @@ void set_events_option(Config *cfg, char *value) {
       break;
     }
   }
+
+  return 0;
 }
 
 void debug_config(Config *cfg) {
   if (cfg == NULL) {
-    fprintf(stderr, "[DEBUG]: Configuration struct is null\n");
+    syslog(LOG_DEBUG, "Configuration struct is null\n");
   }
 
-  fprintf(stdout, "[DEBUG]: Paths option settings\n");
-  fprintf(stdout, "[DEBUG]: Number of paths set for monitoring [%d]\n",
-          cfg->paths_size);
+  syslog(LOG_DEBUG, "Paths option settings\n");
+  syslog(LOG_DEBUG, "Number of paths set for monitoring [%d]\n",
+         cfg->paths_size);
 
   for (int i = 0; i < cfg->paths_size; i++) {
-    fprintf(stdout, "[DEBUG]: Path @ index [%d] contains value [%s]\n", i,
-            cfg->paths[i]);
+    syslog(LOG_DEBUG, "Path @ index [%d] contains value [%s]\n", i,
+           cfg->paths[i]);
   }
-  fprintf(stdout, "\n");
+  syslog(LOG_DEBUG, "\n");
 
-  fprintf(stdout, "[DEBUG]: File event option settings\n");
+  syslog(LOG_DEBUG, "File event option settings\n");
   if (cfg->events_bmask == 0) {
-    fprintf(stderr, "[WARNING]: File event flags are not enabled\n");
+    syslog(LOG_DEBUG, "File event flags are not enabled\n");
     return;
   } else {
-    fprintf(stdout, "[DEBUG]: File Event Bit Flags [0x%08X]\n",
+    syslog(LOG_DEBUG, "File Event Bit Flags [0x%08X]\n",
             cfg->events_bmask);
   }
 
   if (cfg->events_bmask & FLAG_ACCESS) {
-    fprintf(stdout, "[DEBUG]: File access event flag enabled\n");
+    syslog(LOG_DEBUG, "File access event flag enabled\n");
   }
 
   if (cfg->events_bmask & FLAG_MODIFY) {
-    fprintf(stdout, "[DEBUG]: File modify event flag enabled\n");
+    syslog(LOG_DEBUG, "File modify event flag enabled\n");
   }
 
   if (cfg->events_bmask & FLAG_MOVE) {
-    fprintf(stdout, "[DEBUG]: File move event flag enabled\n");
+    syslog(LOG_DEBUG, "File move event flag enabled\n");
   }
 
   if (cfg->events_bmask & FLAG_CLOSE) {
-    fprintf(stdout, "[DEBUG]: File close event flag enabled\n");
+    syslog(LOG_DEBUG, "File close event flag enabled\n");
   }
 }
